04c14b6ab8a71ab4b98b59252621401e18996f29
jingkai27 <jingkai.t27@gmail.com>
2024-01-05 15:01:14 +0800

    learning java
    learnt about functions, methods in java today

---
+++ hangman_game/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("before sayHi");
+        sayHi();
+    }
+
+    public static void sayHi() {
+        System.out.println("Hello from Jing Kai.");
+    }
+
+}
